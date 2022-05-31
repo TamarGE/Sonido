d753556ed1d01215c5590b967895b5a4976f1d54
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-05-31 11:47:44 -0300

    Initial commit

---
+++ Assets/Sound/PlaySound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioClip sound1;
+    public AudioClip sound2;
+
+    AudioSource fuenteSonido;
+
+    void Start()
+    {
+        fuenteSonido = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            fuenteSonido.clip = sound1;
+            fuenteSonido.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            fuenteSonido.clip = sound2;
+            fuenteSonido.Play();
+        }
+    }
+}
